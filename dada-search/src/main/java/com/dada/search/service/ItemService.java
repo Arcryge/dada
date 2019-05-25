88327343e6a969b5cd00f8c45e3c7c94e1235e37
Arcry <arcryge@163.com>
2019-05-25 15:58:18 +0800

    service层

---
+++ dada-search/src/main/java/com/dada/search/service/ItemService.java
@@ -0,0 +1,13 @@
+/**
+ * @author Arcry
+ * @date 2019年5月25日
+ * @time 下午3:47:42
+ */
+package com.dada.search.service;
+
+import com.dada.common.pojo.DadaResult;
+
+public interface ItemService {
+
+	DadaResult importAllItems();
+}
