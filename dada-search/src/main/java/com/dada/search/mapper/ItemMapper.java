41cfaed21f1755c42a6533c71f353a148920d042
Arcry <arcryge@163.com>
2019-05-25 15:41:10 +0800

    dao层

---
+++ dada-search/src/main/java/com/dada/search/mapper/ItemMapper.java
@@ -0,0 +1,15 @@
+/**
+ * @author Arcry
+ * @date 2019年5月25日
+ * @time 下午3:32:51
+ */
+package com.dada.search.mapper;
+
+import java.util.List;
+
+import com.dada.search.pojo.Item;
+
+public interface ItemMapper {
+
+	List<Item> getItemList();
+}
