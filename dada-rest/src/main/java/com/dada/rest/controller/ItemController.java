e15854c48e029a4c4bf3514ffa42d60d726b1874
Arcry <arcryge@163.com>
2019-05-26 18:18:58 +0800

    商品基本信息服务

---
+++ dada-rest/src/main/java/com/dada/rest/controller/ItemController.java
@@ -0,0 +1,38 @@
+/**
+ * @author Arcry
+ * @date 2019年5月26日
+ * @time 下午6:13:49
+ */
+package com.dada.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dada.common.pojo.DadaResult;
+import com.dada.rest.service.ItemService;
+
+/**
+ * 商品信息controller
+ *  
+ * @author Arcry
+ * @version 0.1
+ * @date 2019年5月26日 下午6:14:02
+ */
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+
+	@RequestMapping("/info/{itemId}")
+	@ResponseBody
+	public DadaResult getItemBaseInfo(@PathVariable Long itemId) {
+		DadaResult result = itemService.getItemBaseInfo(itemId);
+		return result;
+	}
+
+}
---
+++ dada-rest/src/main/java/com/dada/rest/service/ItemService.java
@@ -0,0 +1,13 @@
+/**
+ * @author Arcry
+ * @date 2019年5月26日
+ * @time 下午6:08:54
+ */
+package com.dada.rest.service;
+
+import com.dada.common.pojo.DadaResult;
+
+public interface ItemService {
+
+	DadaResult getItemBaseInfo(long itemId);
+}
