5124064b2061953acda3e9b8b70588fd22ba803c
Arcry <arcryge@163.com>
2019-05-27 23:40:54 +0800

    用户注册功能实现

---
+++ dada-sso/src/main/java/com/dada/sso/controller/PageController.java
@@ -0,0 +1,26 @@
+/**
+ * @author Arcry
+ * @date 2019年5月27日
+ * @time 下午11:23:53
+ */
+package com.dada.sso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面跳转controller
+ *  
+ * @author Arcry
+ * @version 0.1
+ * @date 2019年5月27日 下午11:23:58
+ */
+@Controller
+@RequestMapping("/page")
+public class PageController {
+
+	@RequestMapping("/register")
+	public String showRegister() {
+		return "register";
+	}
+}
