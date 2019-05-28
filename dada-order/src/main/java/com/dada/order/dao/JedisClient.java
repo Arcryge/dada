e68a8df05fe2b296aab6138293f4e28b13fc9249
Arcry <arcryge@163.com>
2019-05-28 21:25:37 +0800

    创建订单系统框架整合

---
+++ dada-order/src/main/java/com/dada/order/dao/JedisClient.java
@@ -0,0 +1,28 @@
+/**
+ * @author Arcry
+ * @date 2019年5月24日
+ * @time 上午9:36:34
+ */
+package com.dada.order.dao;
+
+public interface JedisClient {
+
+	String get(String key);
+
+	String set(String key, String value);
+
+	String hget(String hkey, String key);
+
+	long hset(String hkey, String key, String value);
+
+	long incr(String key);
+
+	long expire(String key, int second);
+
+	long ttl(String key);
+
+	long del(String key);
+
+	long hdel(String hkey, String key);
+
+}
