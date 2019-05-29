ae819e70e93f1e6c835da516f184221d614a8465
Arcry <arcryge@163.com>
2019-05-29 10:51:00 +0800

    创建订单完成

--- dada-portal/src/main/java/com/dada/portal/controller/OrderController.java
+++ dada-portal/src/main/java/com/dada/portal/controller/OrderController.java
@@ -10,13 +10,16 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dada.portal.pojo.CartItem;
+import com.dada.portal.pojo.Order;
 import com.dada.portal.service.CartService;
+import com.dada.portal.service.OrderService;
 
 @Controller
 @RequestMapping("/order")
@@ -24,6 +27,9 @@ public class OrderController {
 
 	@Autowired
 	private CartService cartService;
+
+	@Autowired
+	private OrderService orderService;
 
 	@RequestMapping("/order-cart")
 	public String showOrderCart(HttpServletRequest request, HttpServletResponse response, Model model) {
@@ -34,5 +40,21 @@ public class OrderController {
 
 		return "order-cart";
 	}
+
+	@RequestMapping("/create")
+	public String createOrder(Order order, Model model) {
+		try {
+			String orderId = orderService.createOrder(order);
+			model.addAttribute("orderId", orderId);
+			model.addAttribute("payment", order.getPayment());
+			model.addAttribute("date", new DateTime().plusDays(3).toString("yyyy-MM-dd"));
+			return "success";
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("message", "创建订单出错。请稍后重试！");
+			return "error/exception";
+		}
+	}
+
 }
 
---
+++ dada-portal/src/main/java/com/dada/portal/service/OrderService.java
@@ -0,0 +1,13 @@
+/**
+ * @author Arcry
+ * @date 2019年5月29日
+ * @time 上午10:11:53
+ */
+package com.dada.portal.service;
+
+import com.dada.portal.pojo.Order;
+
+public interface OrderService {
+
+	String createOrder(Order order);
+}
