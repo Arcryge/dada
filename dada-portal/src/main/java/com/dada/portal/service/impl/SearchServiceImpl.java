7a6a9143b3b205920d2ecb432d21146ecd1e46cc
Arcry <arcryge@163.com>
2019-06-03 15:33:58 +0800

    登录和注册页面修改

--- dada-portal/src/main/java/com/dada/portal/service/impl/SearchServiceImpl.java
+++ dada-portal/src/main/java/com/dada/portal/service/impl/SearchServiceImpl.java
@@ -31,7 +31,7 @@ public class SearchServiceImpl implements SearchService {
 
 	@Override
 	public SearchResult search(String queryString, int page) {
-		// 调用taotao-search的服务
+		// 调用dada-search的服务
 		//查询参数
 		Map<String, String> param = new HashMap<>();
 		param.put("q", queryString);
@@ -40,9 +40,9 @@ public class SearchServiceImpl implements SearchService {
 			//调用服务
 			String json = HttpClientUtil.doGet(SEARCH_BASE_URL, param);
 			//把字符串转换成java对象
-			DadaResult taotaoResult = DadaResult.formatToPojo(json, SearchResult.class);
-			if (taotaoResult.getStatus() == 200) {
-				SearchResult result = (SearchResult) taotaoResult.getData();
+			DadaResult dadaResult = DadaResult.formatToPojo(json, SearchResult.class);
+			if (dadaResult.getStatus() == 200) {
+				SearchResult result = (SearchResult) dadaResult.getData();
 				return result;
 			}
 
